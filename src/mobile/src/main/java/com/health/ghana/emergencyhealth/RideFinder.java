6c3313fdc3eb4772d5025e53a8968b792ee7d4a8
Sam <Sam@dhcp-hol-1193.redrover.cornell.edu>
2017-11-15 22:39:58 -0500

    Android App Starter Code

---
+++ src/mobile/src/main/java/com/health/ghana/emergencyhealth/RideFinder.java
@@ -0,0 +1,46 @@
+package com.health.ghana.emergencyhealth;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class RideFinder extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_finder);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Accra, Ghana.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Accra and move the camera
+        LatLng accra = new LatLng(5.55602,  -0.1969);
+        mMap.addMarker(new MarkerOptions().position(accra).title("Marker in Accra"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(accra));
+    }
+}
